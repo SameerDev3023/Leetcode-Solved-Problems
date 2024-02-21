02c4d57229662ebcbf58472066b3b400b601f461
sameer <sameerdeveloper05@gmail.com>
2024-02-21 22:04:53 +0530

    Rhombus Star Pattern

---
+++ StarPatterns/RhombusPattern.java
@@ -0,0 +1,22 @@
+package StarPatterns;
+
+public class RhombusPattern {
+    public static void main(String args[]){
+
+
+        int Row = 8;
+        int Col = 8;
+        for(int i = 1;i<=Row;i++){
+            for(int j = 1;j<=i;j++){
+                System.out.print("  ");
+            }   
+            for(int k = 1;k<=Col;k++){
+                System.out.print("*");
+            } 
+            System.out.println();
+        }
+       
+
+    }
+    
+}
